bd996f1689fa91d1ec2b71a03542f774b09927ed
anhkhoatranle30 <anhkhoatranle30@gmail.com>
2022-07-04 11:47:47 +0700

    feat: Clamp pitch angle

--- src/GameClient/Assets/Scripts/Player/CameraRotation.cs
+++ src/GameClient/Assets/Scripts/Player/CameraRotation.cs
@@ -6,7 +6,13 @@ using UnityEngine;
 public class CameraRotation : MonoBehaviour
 {
     [SerializeField]
+    private float _upperLimitAngle;
+    [SerializeField]
+    private float _lowerLimitAngle;
+    [SerializeField]
     private Transform _playerCamera;
+
+    const float FullCircleDegree = 2 * Mathf.PI * Mathf.Rad2Deg;
 
     private void Update()
     {
@@ -23,6 +29,36 @@ public class CameraRotation : MonoBehaviour
     private void UpdatePitch()
     {
         float pitch = Input.GetAxis("Mouse Y");
+        ClampPitch(ref pitch);
         _playerCamera.transform.Rotate(-pitch, 0, 0);
+    }
+
+    private void ClampPitch(ref float pitch)
+    {
+        float localXAngle = _playerCamera.transform.localEulerAngles.x;
+        ClampUpperPitch(ref pitch, localXAngle);
+        ClampLowerPitch(ref pitch, localXAngle);
+    }
+
+    private void ClampUpperPitch(ref float pitch, float localXAngle)
+    {
+        if (localXAngle > FullCircleDegree / 2)
+        {
+            if ((localXAngle < FullCircleDegree - _upperLimitAngle) && pitch > 0)
+            {
+                pitch = 0;
+            }
+        }
+    }
+
+    private void ClampLowerPitch(ref float pitch, float localXAngle)
+    {
+        if (localXAngle < FullCircleDegree / 2)
+        {
+            if (localXAngle > -_lowerLimitAngle && pitch < 0)
+            {
+                pitch = 0;
+            }
+        }
     }
 }
