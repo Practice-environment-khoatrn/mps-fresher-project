ed3dfbf5d2d715725f75904c2a1b018128bfd50a
anhkhoatranle30 <anhkhoatranle30@gmail.com>
2022-07-04 10:54:04 +0700

    feat: Update pitch rotation

--- src/GameClient/Assets/Scripts/Player/CameraRotation.cs
+++ src/GameClient/Assets/Scripts/Player/CameraRotation.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
 {
+    [SerializeField]
+    private Transform _playerCamera;
+
     private void Update()
     {
         UpdateYaw();
+        UpdatePitch();
     }
 
     private void UpdateYaw()
     {
         float yaw = Input.GetAxis("Mouse X");
         transform.Rotate(0, yaw, 0);
+    }
+
+    private void UpdatePitch()
+    {
+        float pitch = Input.GetAxis("Mouse Y");
+        _playerCamera.transform.Rotate(-pitch, 0, 0);
     }
 }
